d2bbc518701e67c4f52a14e288bbfbab1a37e3e9
RafatMeraz <razhossain201@gmail.com>
2026-02-10 22:50:56 +0600

    Initial Commit

---
+++ crafty_bay/README.md
@@ -0,0 +1,7 @@
+# crafty_bay
+
+Flutter e-commerce project for Ostad batch thirteen
+
+## Set Up
+
+Flutter Version - 3.38.3
---
+++ crafty_bay/android/app/src/main/kotlin/com/ostad_batch_thirteen/crafty_bay/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ostad_batch_thirteen.crafty_bay
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
