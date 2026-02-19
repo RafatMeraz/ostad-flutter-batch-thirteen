36e3c7584c108337ab645e0c71740d6340ec7666
RafatMeraz <razhossain201@gmail.com>
2026-02-19 23:15:47 +0600

    Add sign up screen ui

--- crafty_bay/lib/app/app_theme.dart
+++ crafty_bay/lib/app/app_theme.dart
@@ -7,17 +7,56 @@ class AppTheme {
     colorSchemeSeed: AppColors.themeColor,
     scaffoldBackgroundColor: Colors.white,
     progressIndicatorTheme: _progressThemeData,
+    textTheme: _textTheme,
+    inputDecorationTheme: _inputDecorationTheme,
+    filledButtonTheme: _filledButtonTheme,
   );
 
   static final ThemeData _darkThemeData = ThemeData(
     colorSchemeSeed: AppColors.themeColor,
     brightness: Brightness.dark,
     progressIndicatorTheme: _progressThemeData,
+    textTheme: _textTheme,
+    inputDecorationTheme: _inputDecorationTheme,
+    filledButtonTheme: _filledButtonTheme,
   );
 
   static ProgressIndicatorThemeData get _progressThemeData {
     return ProgressIndicatorThemeData(color: AppColors.themeColor);
   }
+
+  static TextTheme get _textTheme =>
+      TextTheme(titleLarge: TextStyle(fontSize: 24, fontWeight: .bold));
+
+  static InputDecorationTheme get _inputDecorationTheme => InputDecorationTheme(
+    hintStyle: TextStyle(color: Colors.grey),
+    contentPadding: .symmetric(horizontal: 16, vertical: 0),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(color: AppColors.themeColor, width: 1),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(color: AppColors.themeColor, width: 1),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(color: AppColors.themeColor, width: 1),
+    ),
+    errorBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(color: Colors.red, width: 1),
+    ),
+  );
+
+  static FilledButtonThemeData get _filledButtonTheme => FilledButtonThemeData(
+    style: FilledButton.styleFrom(
+      shape: RoundedRectangleBorder(borderRadius: .circular(8)),
+      backgroundColor: AppColors.themeColor,
+      padding: .symmetric(vertical: 12),
+      fixedSize: Size.fromWidth(double.maxFinite),
+    ),
+  );
 
   static ThemeData get lightTheme => _lightThemeData;
 
--- crafty_bay/lib/features/auth/presentation/screens/sign_up_screen.dart
+++ crafty_bay/lib/features/auth/presentation/screens/sign_up_screen.dart
@@ -1,7 +1,7 @@
-import 'package:crafty_bay/features/shared/presentation/widgets/theme_switcher.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../app/extensions/localization_extension.dart';
+import '../../../../app/extensions/utils_extension.dart';
+import '../widgets/app_logo.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -13,15 +13,107 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _firstNameTEController = TextEditingController();
+  final TextEditingController _lastNameTEController = TextEditingController();
+  final TextEditingController _phoneTEController = TextEditingController();
+  final TextEditingController _cityTEController = TextEditingController();
+  final TextEditingController _passwordTEController = TextEditingController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(context.l10n.signUp)),
-      body: Column(
-        children: [
-          ThemeSwitcher()
-        ],
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const .all(24),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  AppLogo(width: 100, height: 100),
+                  const SizedBox(height: 16),
+                  Text(
+                    'Create Your Account',
+                    style: context.textTheme.titleLarge,
+                  ),
+                  Text(
+                    'Get started with your details',
+                    style: context.textTheme.bodyLarge?.copyWith(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  TextFormField(
+                    controller: _emailTEController,
+                    keyboardType: .emailAddress,
+                    textInputAction: .next,
+                    decoration: InputDecoration(hintText: 'Email'),
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: _firstNameTEController,
+                    textInputAction: .next,
+                    decoration: InputDecoration(hintText: 'First name'),
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: _lastNameTEController,
+                    textInputAction: .next,
+                    decoration: InputDecoration(hintText: 'Last name'),
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: _phoneTEController,
+                    textInputAction: .next,
+                    decoration: InputDecoration(hintText: 'Phone'),
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: _cityTEController,
+                    textInputAction: .next,
+                    decoration: InputDecoration(hintText: 'City'),
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: _passwordTEController,
+                    obscureText: true,
+                    obscuringCharacter: '*',
+                    decoration: InputDecoration(hintText: 'Password'),
+                  ),
+                  const SizedBox(height: 16),
+                  FilledButton(
+                    onPressed: _onTapSignUpButton,
+                    child: Text('Sign Up'),
+                  ),
+                  const SizedBox(height: 16),
+                  TextButton(
+                    onPressed: _onTapSignInButton,
+                    child: Text('Sign In'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
+  }
+
+  void _onTapSignUpButton() {}
+
+  void _onTapSignInButton() {}
+
+  @override
+  void dispose() {
+    _emailTEController.dispose();
+    _firstNameTEController.dispose();
+    _lastNameTEController.dispose();
+    _phoneTEController.dispose();
+    _cityTEController.dispose();
+    _passwordTEController.dispose();
+    super.dispose();
   }
 }
