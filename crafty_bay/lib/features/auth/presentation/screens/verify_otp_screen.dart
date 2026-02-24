946b81e8a872905500fd0c66e71183b060a3fd4f
RafatMeraz <razhossain201@gmail.com>
2026-02-24 22:43:35 +0600

    Refactor OTP verification screen

--- crafty_bay/lib/features/auth/presentation/screens/verify_otp_screen.dart
+++ crafty_bay/lib/features/auth/presentation/screens/verify_otp_screen.dart
@@ -1,11 +1,10 @@
-import 'dart:async';
-
-import 'package:crafty_bay/app/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
 
+import '../../../../app/app_colors.dart';
 import '../../../../app/extensions/utils_extension.dart';
 import '../widgets/app_logo.dart';
+import '../widgets/resend_otp_section.dart';
 
 class VerifyOtpScreen extends StatefulWidget {
   const VerifyOtpScreen({super.key});
@@ -20,8 +19,6 @@ class _VerifyOtpScreenState extends State<VerifyOtpScreen> {
   final PinInputController _otpTEController = PinInputController();
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
-  Timer? _resendOtpTimer;
 
   @override
   Widget build(BuildContext context) {
@@ -78,86 +75,9 @@ class _VerifyOtpScreenState extends State<VerifyOtpScreen> {
 
   void _onTapSignInButton() {}
 
-  void _onTapSignUpButton() {
-    Navigator.pop(context);
-  }
-
   @override
   void dispose() {
     _otpTEController.dispose();
-    super.dispose();
-  }
-}
-
-class ResendOtpSection extends StatefulWidget {
-  const ResendOtpSection({
-    super.key,
-  });
-
-  @override
-  State<ResendOtpSection> createState() => _ResendOtpSectionState();
-}
-
-class _ResendOtpSectionState extends State<ResendOtpSection> {
-  Timer? _timer;
-  int _start = 30;
-  bool _showResendOtpButton = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _startTimer();
-  }
-
-  void _startTimer() {
-    _start = 30;
-    _showResendOtpButton = false;
-    setState(() {});
-
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      if (_start == 0) {
-        timer.cancel();
-        _showResendOtpButton = true;
-      } else {
-        _start--;
-      }
-      setState(() {});
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        if (!_showResendOtpButton)
-          RichText(
-            text: TextSpan(
-              style: TextStyle(color: Colors.black54),
-              children: [
-                TextSpan(text: 'You can resend OTP after '),
-                TextSpan(
-                  text: '${_start}s',
-                  style: TextStyle(color: AppColors.themeColor, fontWeight: .w600),
-                ),
-              ],
-            ),
-          ),
-        if (_showResendOtpButton)
-          TextButton(
-            onPressed: _onTapSignUpButton,
-            child: Text('Resend OTP'),
-          ),
-      ],
-    );
-  }
-
-  void _onTapSignUpButton() {
-    _startTimer();
-  }
-
-  @override
-  void dispose() {
-    _timer?.cancel();
     super.dispose();
   }
 }
