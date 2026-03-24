e65bbadb0ef1e35e8be7a7ca4a4800317e2efbb0
RafatMeraz <razhossain201@gmail.com>
2026-03-24 17:41:20 +0100

    Add product details ui

--- crafty_bay/lib/features/product/presentation/screens/product_details_screen.dart
+++ crafty_bay/lib/features/product/presentation/screens/product_details_screen.dart
@@ -1,5 +1,7 @@
 import 'package:crafty_bay/app/extensions/utils_extension.dart';
+import 'package:crafty_bay/features/product/presentation/widgets/color_picker.dart';
 import 'package:crafty_bay/features/product/presentation/widgets/product_image_carousel.dart';
+import 'package:crafty_bay/features/product/presentation/widgets/size_picker.dart';
 import 'package:crafty_bay/features/shared/presentation/widgets/inc_dec_button.dart';
 import 'package:flutter/material.dart';
 
@@ -30,6 +32,7 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 16),
                     child: Column(
+                      crossAxisAlignment: .start,
                       children: [
                         const SizedBox(height: 16),
                         Row(
@@ -42,25 +45,33 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
                                 children: [
                                   Text(
                                     'Nike New Shoe DF3434K 2026 Edition',
-                                    style: context.textTheme.bodyLarge?.copyWith(
-                                      fontWeight: .w600
-                                    ),
+                                    style: context.textTheme.bodyLarge
+                                        ?.copyWith(fontWeight: .w600),
                                   ),
                                   Row(
                                     spacing: 8,
                                     children: [
                                       Wrap(
                                         children: [
-                                          Icon(Icons.star, size: 16, color: Colors.amber),
+                                          Icon(
+                                            Icons.star,
+                                            size: 16,
+                                            color: Colors.amber,
+                                          ),
                                           Text('4.5'),
                                         ],
                                       ),
-                                      TextButton(onPressed: () {}, child: Text('Reviews')),
+                                      TextButton(
+                                        onPressed: () {},
+                                        child: Text('Reviews'),
+                                      ),
                                       Container(
                                         padding: .all(4),
                                         decoration: BoxDecoration(
                                           color: AppColors.themeColor,
-                                          borderRadius: BorderRadius.circular(8),
+                                          borderRadius: BorderRadius.circular(
+                                            8,
+                                          ),
                                         ),
                                         child: Icon(
                                           Icons.favorite_outline,
@@ -76,6 +87,31 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
                             IncDecButton(onChange: (int value) {}),
                           ],
                         ),
+                        const SizedBox(height: 16),
+                        ColorPicker(
+                          initialValue: 'Red',
+                          colors: ['Red', 'Green', 'Black'],
+                          onSelected: (String selectedColor) {},
+                        ),
+                        const SizedBox(height: 16),
+                        SizePicker(
+                          initialValue: 'L',
+                          sizes: ['S', 'M', 'L', 'XL', 'XXL'],
+                          onSelected: (String selectedColor) {},
+                        ),
+                        const SizedBox(height: 16),
+                        Text(
+                          'Description',
+                          style: context.textTheme.bodyLarge?.copyWith(fontWeight: .bold),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          '''Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's''',
+                          style: TextStyle(
+                            color: Colors.black54,
+                          ),
+                        ),
+                        const SizedBox(height: 16),
                       ],
                     ),
                   ),
