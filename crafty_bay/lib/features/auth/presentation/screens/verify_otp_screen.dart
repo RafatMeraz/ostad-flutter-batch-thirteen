8d52398d7a334e79ef47d7aae7f17665f4dc8e36
RafatMeraz <razhossain201@gmail.com>
2026-04-07 18:48:20 +0200

    Implement verify otp api

--- crafty_bay/lib/features/auth/presentation/screens/verify_otp_screen.dart
+++ crafty_bay/lib/features/auth/presentation/screens/verify_otp_screen.dart
@@ -1,5 +1,10 @@
+import 'package:crafty_bay/features/auth/data/models/verify_otp_params.dart';
+import 'package:crafty_bay/features/auth/presentation/providers/verify_otp_provider.dart';
+import 'package:crafty_bay/features/shared/presentation/screens/main_nav_holder_screen.dart';
+import 'package:crafty_bay/features/shared/presentation/widgets/snack_bar_message.dart';
 import 'package:flutter/material.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
+import 'package:provider/provider.dart';
 
 import '../../../../app/app_colors.dart';
 import '../../../../app/extensions/utils_extension.dart';
@@ -7,9 +12,11 @@ import '../widgets/app_logo.dart';
 import '../widgets/resend_otp_section.dart';
 
 class VerifyOtpScreen extends StatefulWidget {
-  const VerifyOtpScreen({super.key});
+  const VerifyOtpScreen({super.key, required this.email});
 
   static const String name = '/verify-otp';
+
+  final String email;
 
   @override
   State<VerifyOtpScreen> createState() => _VerifyOtpScreenState();
@@ -20,51 +27,56 @@ class _VerifyOtpScreenState extends State<VerifyOtpScreen> {
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
+  final VerifyOtpProvider _verifyOtpProvider = VerifyOtpProvider();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Padding(
-            padding: const .all(24),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  const SizedBox(height: 48),
-                  AppLogo(width: 100, height: 100),
-                  const SizedBox(height: 16),
-                  Text('Enter OTP Code', style: context.textTheme.titleLarge),
-                  Text(
-                    'A 4 digit otp has been sent to your email address',
-                    style: context.textTheme.bodyLarge?.copyWith(
-                      color: Colors.grey,
+    return ChangeNotifierProvider.value(
+      value: _verifyOtpProvider,
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Padding(
+              padding: const .all(24),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    const SizedBox(height: 48),
+                    AppLogo(width: 100, height: 100),
+                    const SizedBox(height: 16),
+                    Text('Enter OTP Code', style: context.textTheme.titleLarge),
+                    Text(
+                      'A 4 digit otp has been sent to your email address',
+                      style: context.textTheme.bodyLarge?.copyWith(
+                        color: Colors.grey,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 24),
-                  MaterialPinField(
-                    length: 4,
-                    pinController: _otpTEController,
-                    keyboardType: TextInputType.number,
-                    textInputAction: TextInputAction.done,
-                    theme: MaterialPinTheme(
-                      shape: MaterialPinShape.outlined,
-                      fillColor: Colors.transparent,
-                      borderColor: AppColors.themeColor,
-                      completeFillColor: Colors.transparent,
-                      completeBorderColor: AppColors.themeColor,
-                      spacing: 16,
-                      cellSize: Size(50, 50),
+                    const SizedBox(height: 24),
+                    MaterialPinField(
+                      length: 4,
+                      pinController: _otpTEController,
+                      keyboardType: TextInputType.number,
+                      textInputAction: TextInputAction.done,
+                      theme: MaterialPinTheme(
+                        shape: MaterialPinShape.outlined,
+                        fillColor: Colors.transparent,
+                        borderColor: AppColors.themeColor,
+                        completeFillColor: Colors.transparent,
+                        completeBorderColor: AppColors.themeColor,
+                        spacing: 16,
+                        cellSize: Size(50, 50),
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 16),
-                  FilledButton(
-                    onPressed: _onTapSignInButton,
-                    child: Text('Verify'),
-                  ),
-                  const SizedBox(height: 16),
-                  ResendOtpSection(),
-                ],
+                    const SizedBox(height: 16),
+                    FilledButton(
+                      onPressed: _onTapVerifyButton,
+                      child: Text('Verify'),
+                    ),
+                    const SizedBox(height: 16),
+                    ResendOtpSection(),
+                  ],
+                ),
               ),
             ),
           ),
@@ -73,7 +85,28 @@ class _VerifyOtpScreenState extends State<VerifyOtpScreen> {
     );
   }
 
-  void _onTapSignInButton() {}
+  void _onTapVerifyButton() {
+    if (_otpTEController.text.length == 4) {
+      _verifyOtp();
+    }
+  }
+
+  Future<void> _verifyOtp() async {
+    VerifyOtpParams params = VerifyOtpParams(
+      email: widget.email,
+      otp: _otpTEController.text,
+    );
+    final bool isSuccess = await _verifyOtpProvider.verifyOtp(params);
+    if (isSuccess) {
+      Navigator.pushNamedAndRemoveUntil(
+        context,
+        MainNavHolderScreen.name,
+        (predicate) => false,
+      );
+    } else {
+      showSnackBarMessage(context, _verifyOtpProvider.errorMessage!);
+    }
+  }
 
   @override
   void dispose() {
